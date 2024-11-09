407b49c87268fafd26331138805f454e257f2143
SharkyBytes <garvagarwal.work@gmail.com>
2024-11-09 12:03:03 +0530

    first commit

---
+++ streamlit_particle_segmentation.py
@@ -0,0 +1,77 @@
+import streamlit as st
+import cv2
+import numpy as np
+
+# Title of my Streamlit app
+st.title('Particle Segmentation App')
+
+# Apply custom CSS to change the background color to black
+
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: black;
+        color: white;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# File uploader widget to upload an image
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+
+if uploaded_file is not None:
+    # Load the uploaded image
+    image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
+    # resizing
+    image = cv2.resize(image, (612, 433))
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Apply thresholding
+    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+    # Apply morphological operations to clean the image
+    kernel = np.ones((3, 3), np.uint8)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
+    sure_bg = cv2.dilate(opening, kernel, iterations=3)
+
+    # Find sure foreground area
+    dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+    _, sure_fg = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
+
+    # Find unknown region
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv2.subtract(sure_bg, sure_fg)
+
+    # Marker labeling
+    _, markers = cv2.connectedComponents(sure_fg)
+
+    # Add 1 to markers to avoid labeling the background as 0
+    markers = markers + 1
+
+    # Mark unknown region as 0 in markers
+    markers[unknown == 255] = 0
+
+    # Apply watershed algorithm
+    cv2.watershed(image, markers)
+    markers[markers == -1] = 0 
+     # Remove watershed lines
+
+    # Count the number of particles (excluding background)
+    num_particles = np.max(markers) - 1
+
+    # st.write(f"Number of particles: {num_particles}")
+
+       # Display the number of particles in the center, bold, and with a larger font size
+    st.markdown(f"<p style='text-align:center;font-size:24px;font-weight:bold;'>Number of particles: {num_particles}</p>", unsafe_allow_html=True)
+
+
+    # Draw boundaries on the original image
+    image[markers > 1] = [0, 0, 255]  # Mark boundaries in red
+
+    # Display the result
+    st.image(image, channels="BGR")
+    
